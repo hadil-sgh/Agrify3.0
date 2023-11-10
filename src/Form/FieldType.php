2017958568c7ccb75317e66c1cb94c6b4ba713e2
gheiythtba <tbagheiyth@gmail.com>
2023-11-10 10:28:46 +0100

    Delete Template

---
+++ src/Form/FieldType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Field;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FieldType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('field_id')
+            ->add('field_Nom')
+            ->add('field_chef')
+            ->add('field_type')
+            ->add('field_Superficie')
+            ->add('field_quantity')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Field::class,
+        ]);
+    }
+}
