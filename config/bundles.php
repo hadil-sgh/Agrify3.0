3e5be2b1a32ea3cfa82a1c5d63f259a06589430d
gheiythtba <tbagheiyth@gmail.com>
2023-11-12 02:26:33 +0100

    Resolve problems with show/edit functions + change symfony v to 8 + add the Package sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
