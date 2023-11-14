39d4844c53118839c4315070448bff3e66db65fe
NourAchahlaou <achahlaou.nour@gmail.com>
2023-11-14 16:14:28 +0100

    add controllers (nutritionalvalue and nutritionalneeds)

---
+++ src/Controller/NutritionalNeedsController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\NutritionalNeeds;
+use App\Form\NutritionalNeeds1Type;
+use App\Repository\NutritionalNeedsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/nutritional/needs')]
+class NutritionalNeedsController extends AbstractController
+{
+    #[Route('/', name: 'app_nutritional_needs_index', methods: ['GET'])]
+    public function index(NutritionalNeedsRepository $nutritionalNeedsRepository): Response
+    {
+        return $this->render('nutritional_needs/index.html.twig', [
+            'nutritional_needs' => $nutritionalNeedsRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_nutritional_needs_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $nutritionalNeed = new NutritionalNeeds();
+        $form = $this->createForm(NutritionalNeeds1Type::class, $nutritionalNeed);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($nutritionalNeed);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_nutritional_needs_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('nutritional_needs/new.html.twig', [
+            'nutritional_need' => $nutritionalNeed,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_nutritional_needs_show', methods: ['GET'])]
+    public function show(NutritionalNeeds $nutritionalNeed): Response
+    {
+        return $this->render('nutritional_needs/show.html.twig', [
+            'nutritional_need' => $nutritionalNeed,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_nutritional_needs_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, NutritionalNeeds $nutritionalNeed, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(NutritionalNeeds1Type::class, $nutritionalNeed);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_nutritional_needs_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('nutritional_needs/edit.html.twig', [
+            'nutritional_need' => $nutritionalNeed,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_nutritional_needs_delete', methods: ['POST'])]
+    public function delete(Request $request, NutritionalNeeds $nutritionalNeed, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$nutritionalNeed->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($nutritionalNeed);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_nutritional_needs_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/NutritionalValueType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\NutritionalValue;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NutritionalValueType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pb')
+            ->add('fb')
+            ->add('adf')
+            ->add('ndf')
+            ->add('ms')
+            ->add('eb')
+            ->add('ca')
+            ->add('p')
+            ->add('mg')
+            ->add('k')
+            ->add('ingredient')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => NutritionalValue::class,
+        ]);
+    }
+}
