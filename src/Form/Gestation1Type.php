7265a8900a096c5ed70e3570118896d4e2d5d4e4
NourAchahlaou <achahlaou.nour@gmail.com>
2023-11-19 21:25:47 +0100

    ajouter les controle de saisie

--- src/Form/Gestation1Type.php
+++ src/Form/Gestation1Type.php
@@ -6,24 +6,52 @@ use App\Entity\Gestation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType; 
-
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 class Gestation1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('espece')
-            ->add('preparationVêlage')
-            ->add('vêlagePrévu')
-            ->add('dateAvertissement')
+            ->add('espece', ChoiceType::class, [
+                'choices' => [
+                    'female' => 'female',
+                    'male' => 'male',
+                ], 
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'it cannot be blank.']),
+                    new Assert\Length(['max' => 255]),
+                ],
+            ])
+            ->add('preparationVelage', DateType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'it cannot be blank.']),
+                    new Assert\Date(),
+                ],
+            ])
+            ->add('velagePrevu', DateType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'it cannot be blank.']),
+                    new Assert\Date(),
+                ],
+            ])
+            ->add('dateAvertissement', DateType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'it cannot be blank.']),
+                    new Assert\Date(),
+                ],
+            ])
             ->add('animal', EntityType::class, [
                 'class' => 'App\Entity\Animal',
-                'choice_label' => 'espece', // Change to the property you want to display
+                'choice_label' => 'espece',
                 'placeholder' => 'Select Animal',
-            ])
-            
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
